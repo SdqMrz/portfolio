8db957ed11ab54b65fcd7103c88c02556a9fed57
sdq <sediqemirzaie@gmail.com>
2022-04-23 00:08:42 +0430

    Add ProjectsViewComponent

---
+++ portfolio_project/Models/Project.cs
@@ -0,0 +1,21 @@
+﻿namespace portfolio_project.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string  Image { get; set; }
+        public string Client { get; set; }
+        public string Description { get; set; }
+
+        public Project(long id, string name, string description, string image, string client)
+        {
+            Id= id;
+            Name= name;
+            Image= image;
+            Client= client;
+            Description= description;
+
+        }
+    }
+}
---
+++ portfolio_project/ViewComponents/ProjectsViewCompomnent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using portfolio_project.Models;
+using System.Collections.Generic;
+
+namespace portfolio_project.ViewComponents
+{
+    public class ProjectsViewCompomnent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string name)
+        {
+            var projects = new List<Project>
+            {
+                new Project(1, "تاکسی", "درخواست آنلاین تاکسی برای سفر های درون شهری", "project-1.jpg", "Atriya"),
+                new Project(2, "زودفود", "درخواست آنلاین غذا برای سراسر کشور", "project-2.jpg","ZoodFood"),
+                new Project(3, "مدارس", "سیستم مدیریت یکپارچه مدارس", "project-3.jpg","MONOP"),
+                new Project(4, "فضاپیما", "برنامه مدیریت فضاپیما های ناسا","project-4.jpg", "NASA"),
+            };
+
+            return View("_Projects",projects);
+        }
+    }
+}
