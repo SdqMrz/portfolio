9165ecb66a19258831026cb64f8a63fa516bfdcd
sdq <sediqemirzaie@gmail.com>
2022-05-03 07:15:32 +0430

    Add projectDetails action and view

--- portfolio_project/Controllers/HomeController.cs
+++ portfolio_project/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using portfolio_project.Models;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace portfolio_project.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly List<Service> _services = new List<Service>
+            {
+             new Service{Id=1,Name="طلایی"},
+             new Service{Id=1,Name="نقره ای"},
+             new Service{Id=1,Name="پلاتین"},
+             new Service{Id=1,Name="الماس"}
+             };
+
 
         public HomeController()
         {
@@ -19,21 +29,41 @@ namespace portfolio_project.Controllers
         [HttpGet]
         public IActionResult Contact()
         {
-            return View();
+            var model = new Contact
+            {
+                Services = new SelectList(_services, "Id", "Name")
+            };
+            return View(model);
         }
 
         [HttpPost]
         public IActionResult Contact(Contact model)
         {
-            if(!ModelState.IsValid)
+            model.Services = new SelectList(_services, "Id", "Name");
+            if (!ModelState.IsValid)
             {
                 ViewBag.error = "اطلاعات وارد شده، صحیح نمیباشد، لطفا دوباره تلاش کنید.";
                 return View(model);
             }
             ViewBag.success = "اطلاعات با موفقیت ارسال شد.";
 
-            return View();
+            ModelState.Clear();
+
+            model = new Contact
+            {
+                Services = new SelectList(_services, "Id", "Name")
+            };
+
+            return View(model);
         }
+
+        public IActionResult ProjectDetails(long id)
+        {
+            var project = Data.ProjectStore.GetProjectBy(id);
+
+            return View(project);
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ portfolio_project/Models/Service.cs
@@ -0,0 +1,8 @@
+﻿namespace portfolio_project.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
